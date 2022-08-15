05eda1ec14b3d7c23ab52dd0fcb799f46a6faecb
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-08-15 20:59:22 -0300

    Update

---
+++ Alura/Bytebank/Bytebank/ContaCorrente.cs
@@ -0,0 +1,125 @@
+﻿using bytebank.Titular;
+
+namespace bytebank
+{
+    
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public string NomeAgencia { get; set; }
+        private string _conta;
+        private int _numeroAgencia;
+        private double saldo;
+
+        public string Conta
+        {
+            get
+            {
+                return _conta;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+                else
+                {
+                    _conta = value;
+                }
+            }
+
+        }
+
+        public int NumeroAgencia {
+            get
+            {
+                return _numeroAgencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _numeroAgencia = value;
+                }
+            }
+                
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            else if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            
+        }
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+        
+
+        public ContaCorrente(int numeroAgencia, string conta)
+        {
+            NumeroAgencia = numeroAgencia;
+            Conta = conta;
+            TotalDeContasCriadas += 1;
+        }
+
+        public static int TotalDeContasCriadas { get; set; }
+
+    }
+
+}
+
---
+++ Alura/Bytebank/Bytebank/Program.cs
@@ -0,0 +1,52 @@
+﻿using bytebank;
+using bytebank.Titular;
+
+Console.WriteLine("Boas Vindas ao seu Banco, ByteBank!");
+
+//ContaCorrente conta1 = new ContaCorrente();
+
+//conta1.titular = "Erick Pedrosa";
+//conta1.conta = "10123-x";
+//conta1.numeroAgencia = 23;
+//conta1.nomeAgencia = "Agência Central";
+//conta1.saldo = 93020.59;
+
+//ContaCorrente conta2 = new ContaCorrente();
+
+//conta2.titular = "Amanda Nogueira";
+//conta2.conta = "11006-X";
+//conta2.numeroAgencia = 23;
+//conta2.nomeAgencia = "Agência Central";
+//conta2.saldo = 100;
+
+
+//Cliente cliente = new Cliente();
+
+//cliente.nome = "André Silva";
+//cliente.cpf = "123654987-00";
+//cliente.profissao = "Programador C#";
+
+//ContaCorrente conta3 = new ContaCorrente();
+
+//conta3.titular = new Cliente();
+//conta3.conta = "2583691-X";
+//conta3.numeroAgencia = 35;
+//conta3.nomeAgencia = "Agência Central";
+
+//Console.WriteLine(cliente.nome);
+//Console.WriteLine(conta3.titular.nome);
+
+
+//ContaCorrente conta4 = new ContaCorrente(65, "36985201-X");
+
+//conta4.Saldo = 50;
+//conta4.Saldo = -750;
+
+//Console.WriteLine("Saldo: " + conta4.Saldo);
+
+ContaCorrente conta5 = new ContaCorrente(65, "36985201-X");
+ContaCorrente conta6 = new ContaCorrente(77, "83822495-X");
+
+Console.WriteLine("Total de contas: " + ContaCorrente.TotalDeContasCriadas);
+
+Console.ReadKey();
