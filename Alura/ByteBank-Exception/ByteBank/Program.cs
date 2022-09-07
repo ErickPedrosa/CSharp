7db09ce64a233d124be7090e8ba9414f2ff5c746
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-09-07 17:42:11 -0300

    update exceptions

---
+++ Alura/ByteBank-Exception/ByteBank/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine("Não é possível divisão por 0!");
+            }
+            catch (Exception excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }*/
+
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
+                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
+
+                // conta1.Transferir(10000, conta2);
+                conta1.Sacar(10000);
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
+
+                Console.WriteLine(e.InnerException.Message);
+                Console.WriteLine(e.InnerException.StackTrace);
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+        static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
+        }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+}
---
+++ Alura/ByteBank-Exception/ByteBank/SaldoInsuficienteException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public SaldoInsuficienteException() : base() { }
+        public SaldoInsuficienteException(string message) : base(message) { }
+        public SaldoInsuficienteException(string message, Exception inner) : base(message, inner) { }
+        
+
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            : this("Tentativa de saque de " + valorSaque + " com saldo de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
+
+    }
+}
