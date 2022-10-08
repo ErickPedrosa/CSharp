9856339139c73394dc8784c2a79fcb2de0cf271c
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-10-08 10:40:21 -0300

    Up

---
+++ Alura/ByteBankIO/ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,60 @@
+﻿using ByteBankIO;
+using System.Linq.Expressions;
+using System.Text;
+using System;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numBytesLidos = -1;
+
+            var buffer = new byte[1024]; // 1KB
+
+
+            while (numBytesLidos != 0)
+            {
+                buffer = new byte[1024];
+                GC.Collect();
+
+
+                numBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer, numBytesLidos);
+            }
+
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+
+        var utf8 = new UTF8Encoding();
+        string texto = utf8.GetString(buffer, 0, bytesLidos);
+
+        Console.Write(texto);
+
+        /*
+        foreach (var myByte in buffer)
+        {
+            Console.Write(myByte);
+            Console.Write(" ");
+        }
+        */
+
+
+
+    }
+
+}
---
+++ Alura/ByteBankIO/ByteBankIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using ByteBankIO;
+using System.Linq.Expressions;
+using System.Text;
+using System;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var caminhoArquivo = "contas.txt";
+
+        using(var stream = new FileStream(caminhoArquivo, FileMode.Open))
+        {
+            var reader = new StreamReader(stream);
+
+            //var texto = reader.ReadToEnd();
+
+            //int numero = reader.Read();
+
+            while (!reader.EndOfStream)
+            {
+                var linha = reader.ReadLine();
+
+
+                Console.WriteLine(linha);
+
+            }
+
+
+        }
+        
+
+        Console.ReadLine();
+    }
+        
+               
+
+}
