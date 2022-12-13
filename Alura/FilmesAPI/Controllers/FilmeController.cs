2f474d9dc2a339f7f0fe945dd3c3ef6e568e4f76
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-12-12 23:50:03 -0300

    Update

--- Alura/FilmesAPI/Controllers/FilmeController.cs
+++ Alura/FilmesAPI/Controllers/FilmeController.cs
@@ -10,16 +10,28 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
 
     }
 
+    [HttpGet]
+    public IEnumerable<Filme> LeFilmes()
+    {
+        return filmes;
+    }
 
+    [HttpGet("{id}")]
+    public Filme? LeFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
 
 }
